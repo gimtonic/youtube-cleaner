3e67f6a63026d1e73c9aab6b54203b67b09c8b45
gimtonic <gimtonic@rambler.ru>
2025-04-21 00:11:45 +0700

    fix

--- craco.config.ts
+++ craco.config.ts
@@ -26,3 +26,56 @@ const config = {
 };
 
 export default config;
+
+
+// const webpack = require('webpack')
+// const path = require('path')
+// const CopyPlugin = require('copy-webpack-plugin')
+// const srcDir = path.join(__dirname, '..', 'src')
+
+// module.exports = {
+//   entry: {
+//     popup: path.join(srcDir, 'popup.tsx'),
+//     'service-worker': path.join(srcDir, 'service-worker.ts')
+//   },
+//   output: {
+//     path: path.join(__dirname, '../dist'),
+//     filename: '[name].js',
+//     clean: true
+//   },
+//   optimization: {
+//     splitChunks: {
+//       name: 'vendor',
+//       chunks(chunk) {
+//         return chunk.name !== 'background'
+//       }
+//     }
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         use: 'ts-loader',
+//         exclude: /node_modules/
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.js']
+//   },
+//   plugins: [
+//     new CopyPlugin({
+//       patterns: [{ from: '.', to: '.', context: 'public' }],
+//       options: {}
+//     })
+//   ]
+// }
+
+
+// const { merge } = require('webpack-merge');
+// const common = require('./webpack.common.js');
+
+// module.exports = merge(common, {
+//     devtool: 'inline-source-map',
+//     mode: 'development'
+// });
